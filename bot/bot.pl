c5e7236d0d5891dc4858600303cdf83caf40e354
JJ Merelo <jjmerelo@gmail.com>
2016-12-16 18:03:08 +0100

    Resto de cosas necesarias

---
+++ bot/bot.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use OSLNavi_bot;
+
+use v5.14;
+
+my $token = shift || die "No token";
+my $bot = OSLNavi_bot->new( token => $token );
+$bot->think;  # this will block unless there is already an event
+              # loop runningpackage OSLNavi_bot;
+
