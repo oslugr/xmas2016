46455675b757bc4bccb0ecb4d1af128a1dda13e4
JJ Merelo <jjmerelo@gmail.com>
2016-12-17 10:37:00 +0100

    añadido el guardado en fichero

--- bot/OSLNavi_bot.pm
+++ bot/OSLNavi_bot.pm
@@ -2,20 +2,24 @@ package OSLNavi_bot;
 
 use strict;
 use warnings;
-use v5.14;
+use v5.16;
 use Data::Dumper;
+use Tie::File;
 
 use Mojo::Base 'Telegram::Bot::Brain';
- 
+
+has filename => "/tmp/words.csv";
 has 'token';
-has regex     => sub { return qr/(koala|[Nn]avidad|PHP)/};
+has regex    => sub { return qr/(koala|[Nn]avidad|PHP)/};
+
 
 sub check {
   my ($self, $msg) = @_;
-  say Dumper( $msg );
-  say Dumper( $msg->chat );
+##  say Dumper( $msg );
+##  say Dumper( $msg->chat );
 
   # Busca el regex
+  return 0 if !$msg->text;
   if ($msg->text =~ $self->regex ) {
     return 1;
   }
@@ -27,6 +31,10 @@ sub response {
   my ($self, $msg) = @_;
 
   # is this a 1-on-1 ?
+  my ($word) = ( $msg->text =~ $self->regex );
+  tie my @words, 'Tie::File', $self->filename or die "Fichero de palabras roto $!";
+
+  push @words, $word;
   if ($msg->chat->is_user) {
     $self->send_message_to_chat_id($msg->chat->id, "Dijiste la palabra");
   }
@@ -38,6 +46,8 @@ sub response {
 sub init {
     my $self = shift;
     $self->add_listener(\&check, \&response);
+    open my $file, ">", $self->filename;
+    close $file;
 }
 
 "Merry Xmas";
